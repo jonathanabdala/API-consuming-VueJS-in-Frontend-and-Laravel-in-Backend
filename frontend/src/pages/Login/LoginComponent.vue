6cdb27d4cd6e1e419f9b8fce1207781e3d993e6c
jonathan <jonathanabdala@gmail.com>
2021-09-20 01:25:22 -0300

    Commit

--- frontend/src/pages/Login/LoginComponent.vue
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -10,23 +10,23 @@
     </div>
   
 </header>
-<div class="form">
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
+
+    <div class="form">
+     <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">E-mail</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+  <button type="submit" class="btn btn-primary"@click.prevent="login">Entrar</button>
+  </form>
 </div>
 </div>
 </div>
@@ -35,11 +35,21 @@
 
 <script>
 export default {
-    
-        
+    name: 'LoginComponent',
+
+    data(){
+      return{
+
+      }
+    },
+      methods:{
+        login(){
+          this.$router.push({name: 'home'});
+        }
+      }  
     }
 </script>
 
-<style lang="scss" src="./style.scss"scoped/>
+<style lang="scss" src="./style.scss" scoped />
 
-</style>
+
